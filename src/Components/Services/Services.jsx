a60ea231da8d0916048b63ee02b8840321d0040d
arun sandapeta <arunkumarsak7642@gmail.com>
2024-07-04 12:32:48 +0530

    changes on services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -19,28 +19,28 @@ export const Services = ({ Mode }) => {
     {
       cardType: "imageRight",
       imageSource: ProjectTraining,
-      heading: "Classroom Training",
-      content: "Embark on an enriching journey with our Classroom Training program. Immerse yourself in a vibrant educational setting, guided by industry experts. Acquire practical insights and cultivate collaborative knowledge-sharing. Enhance your skills through hands-on training, gearing up for success in the constantly evolving professional landscape",
+      heading: "Project Training",
+      content: "Experience top-notch training with live projects, offering real-time exposure for unparalleled learning. Our Java project training goes beyond theory, focusing on practical application in Java-based projects. Participants gain hands-on experience, honing problem-solving, teamwork, and project management skills, equipping them for success in Java development and related professional challenges.",
     },
     {
       cardType: "imageLeft",
       imageSource: Javafullstack,
-      heading: "Classroom Training",
-      content: "Embark on an enriching journey with our Classroom Training program. Immerse yourself in a vibrant educational setting, guided by industry experts. Acquire practical insights and cultivate collaborative knowledge-sharing. Enhance your skills through hands-on training, gearing up for success in the constantly evolving professional landscape",
+      heading: "Java Fullstack",
+      content: "Embrace excellence in Java Fullstack development with Neoteric Methods, harmoniously integrating both frontend and backend technologies. Enhance user experiences, optimize performance, and deliver robust solutions tailored for a dynamic and modern development landscape.",
       buttonLabel: "Enroll"
     },
     {
       cardType: "imageRight",
       imageSource: SpecialBatches,
-      heading: "Classroom Training",
-      content: "Embark on an enriching journey with our Classroom Training program. Immerse yourself in a vibrant educational setting, guided by industry experts. Acquire practical insights and cultivate collaborative knowledge-sharing. Enhance your skills through hands-on training, gearing up for success in the constantly evolving professional landscape",
+      heading: "Special Batches",
+      content: "Embark on an innovative journey with Neoteric Method’s Exclusive Special Batches! Boost your expertise in Java Full Stack development with individualized coaching, practical projects, and a tailor-made curriculum. Become part of our professional community, unlock your full potential through our specialized approach, and stand out in the ever-evolving tech landscape. Enroll now to sculpt your future with Neoteric Method’s steadfast commitment to excellence and personalized learning experiences designed for corporate success.",
      
     },
     {
       cardType: "imageLeft",
       imageSource: CorporateTraining,
-      heading: "Classroom Training",
-      content: "Embark on an enriching journey with our Classroom Training program. Immerse yourself in a vibrant educational setting, guided by industry experts. Acquire practical insights and cultivate collaborative knowledge-sharing. Enhance your skills through hands-on training, gearing up for success in the constantly evolving professional landscape",
+      heading: "Corporate Training",
+      content: "Corporate training involves a methodical approach where organizations offer employees structured learning opportunities to augment their skills, knowledge, and competencies. Geared towards enhancing both individual and collective performance, corporate training is designed to align with organizational goals, promoting professional development and adaptability in the dynamic business environment to ensure sustained success.",
     
     }
   ];
@@ -66,8 +66,26 @@ export const Services = ({ Mode }) => {
 
       <DataCard cardData={cardData} />
 
-    <div>
+    <div className="NM-container">
       <h1 style={{color:"#F4BF7D"}}>We provide two choices for sessions</h1>
+      
+      <div className="sessions-Box">
+        <div className="session-1">
+          <p>Join sessions at our dedicated venue, offering an alternative environment for an 
+        optimal learning experience.</p>
+        <div>
+          <button type="button" class="btn btn-primary">Offline Sessions</button>
+        </div>
+        
+        </div>
+        <div className="session-2">
+          <p>Convenient on-site sessions conducted at your office.</p>
+        <div className="onsite-btn">
+        <button type="button" class="btn btn-primary">On-site sessions</button>
+        </div>
+        </div>
+        </div>
+      
     </div>
     </>
   );
